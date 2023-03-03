0088416baf47e19267ea158157bd431921ef041f
Samuel Bosawer <zam.bosawer@gmail.com>
2023-03-03 22:14:34 +0900

    Delete UserController Copy

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
-  
+
 use Illuminate\Support\Facades\Route;
-  
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-  
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,11 +17,11 @@ use App\Http\Controllers\ProductController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-  
+
 Route::get('/', function () {
     return view('welcome');
 });
-  
+
 // Auth::routes();
 Auth::routes(['register' => false]);
 Route::redirect('/register', '/login');
@@ -38,8 +38,8 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('products', ProductController::class);
 });
 
- 
+
 
 
 require_once 'app.php';
- 
+
