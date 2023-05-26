b946d66a3427d79e71745acfb3a22c399f443770
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-05-27 07:02:03 +0900

    UPDATED: form kontak

--- resources/views/visitor/pages/kontak.blade.php
+++ resources/views/visitor/pages/kontak.blade.php
@@ -17,9 +17,32 @@
         </div>
     </div>
     <!-- Breadcrumbs End --> 
+    
+    <div class="rs-faq-part style1 pt-100 pb-100 md-pt-70 md-pb-70" style="background-image: url(assets/images/banner/home12/banner-home12.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 padding-0 col-md-12 md-mb-40">
+                    <div>
+                        <img src="{{ asset('gambar/ilustrasi/3.png') }}" alt="gambar ilustrasi">
+                    </div>
+                </div>
+                <div class="col-lg-6 padding-0 col-md-12">
+                    <div class="rs-free-contact">
+                        <div class="sec-title3">
+                            <h2 class="title white-color">Ajukan Pertanyaan Anda</h2>
+                        </div>
 
-    @include('visitor.sections.form-kontak')
-    <!-- Link Form Kontak --> 
+                        @include('visitor.sections.form-kontak')
+                        <!-- Link Form Kontak --> 
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    
 
     @include('visitor.sections.link-terkait')
     <!-- Link Terkait --> 
