c3f9d7e10ae506b46e1531476181f229afd509ab
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-05-26 22:25:45 +0900

    REMOVED: modal search

--- resources/views/visitor/layout/app.blade.php
+++ resources/views/visitor/layout/app.blade.php
@@ -42,27 +42,6 @@
     </div>
     <!-- End scrollUp  -->
 
-
-    <!-- Search Modal Start -->
-    <div class="modal fade search-modal" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel"
-        aria-hidden="true">
-        <button type="button" class="close" data-bs-dismiss="modal">
-            <span class="flaticon-cross"></span>
-        </button>
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="search-block clearfix">
-                    <form>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Search Here..." type="text">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Search Modal End -->
-
     <!-- modernizr js -->
     <script src="{{ asset('assets/visitor/assets/js/modernizr-2.8.3.min.js') }}"></script>
     <!-- jquery latest version -->
