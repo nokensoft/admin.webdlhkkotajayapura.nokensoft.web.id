d120f978177d11386f4e2dbba7166061d079e834
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-05-29 03:21:01 +0900

    UPDATE: link terkait

--- database/seeders/LinkTerkaitSeeder.php
+++ database/seeders/LinkTerkaitSeeder.php
@@ -19,49 +19,51 @@ class LinkTerkaitSeeder extends Seeder
         // Kementerian
         LinkTerkait::create([
             'id'                    => 1,
-            'author'                => 3,
+            'author'                => 1,
             'judul_link'            => 'Kementerian Lingkungan Hidup dan Kehutanan',
             'slug'                  => 'kementerian-lingkungan-hidup-dan-kehutanan',
             'gambar'                => 'gambar/link-terkait/kementerian.png',
             'url'                   => 'https://www.menlhk.go.id',
-
-
         ]);
 
         // Provinsi Papua
         LinkTerkait::create([
             'id'                    => 2,
-            'author'                => 3,
+            'author'                => 1,
             'judul_link'            => 'Pemerintah Daerah Provinsi Papua',
             'slug'                  => 'provinsi-papua',
-            'gambar'                => 'gambar/link-terkait/kota-jayapura.png',
+            'gambar'                => 'gambar/link-terkait/provinsi-papua.png',
             'url'                   => 'https://www.papua.go.id',
-
-
         ]);
 
         // Pemerintah Kota Jayapura
         LinkTerkait::create([
             'id'                    => 3,
-            'author'                => 3,
+            'author'                => 1,
             'judul_link'            => 'Pemerintah Kota Jayapura',
             'slug'                  => 'kota-jayapura',
             'gambar'                => 'gambar/link-terkait/kota-jayapura.png',
             'url'                   => 'https://jayapurakota.go.id/',
-
-
         ]);
 
         // Aplikasi Lapor
         LinkTerkait::create([
             'id'                    => 4,
-            'author'                => 3,
+            'author'                => 1,
             'judul_link'            => 'Aplikasi Lapor',
             'slug'                  => 'aplikasi-lapor',
-            'gambar'                => 'gambar/link-terkait/kota-jayapura.png',
+            'gambar'                => 'gambar/link-terkait/lapor.png',
             'url'                   => 'https://www.lapor.go.id',
+        ]);
 
-
+        // Aplikasi Lapor
+        LinkTerkait::create([
+            'id'                    => 5,
+            'author'                => 1,
+            'judul_link'            => 'LPSE',
+            'slug'                  => 'lpse-kota-jayapura',
+            'gambar'                => 'gambar/link-terkait/lpse.png',
+            'url'                   => 'http://lpse.jayapurakota.go.id/eproc4/',
         ]);
 
 
