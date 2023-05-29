178cc3024c33afc4c2531ff40a0715fc5452c04c
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-05-29 17:28:21 +0900

    UPDATE: url logo footer

--- resources/views/visitor/sections/loader.blade.php
+++ resources/views/visitor/sections/loader.blade.php
@@ -1,7 +1,7 @@
 <div id="loader" class="loader green-color">
     <div class="loader-container">
         <div class='loader-icon'>
-            <img src="{{ asset($pengaturan->logo_loader) }}" alt="Loader">
+            <img src="{{ asset('gambar/pengaturan/' . $pengaturan->logo_loader) }}" alt="Logo Loader">
         </div>
     </div>
 </div>
