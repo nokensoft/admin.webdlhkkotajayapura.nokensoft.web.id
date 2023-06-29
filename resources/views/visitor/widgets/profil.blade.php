c2061206fc047c8913360f717a8d57894de3b805
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-06-29 16:31:50 +0900

    update sidebar profil

--- resources/views/visitor/widgets/profil.blade.php
+++ resources/views/visitor/widgets/profil.blade.php
@@ -1,4 +1,4 @@
-<div class="recent-posts mb-50 text-center" data-tilt data-tilt-reverse="true" data-tilt-scale="1.1" style="will-change: transform; transform: perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1);">
+<div class="recent-posts mb-50 text-center">
     <h3>{{ $pengaturan->judul_situs }}</h3>
     <p>{{ $pengaturan->deskripsi_situs }}</p>
     <img src="{{ asset('gambar/ilustrasi/6.png') }}" alt="gambar ilustrasi" class="mb-3" data-tilt data-tilt-reverse="true" data-tilt-scale="1.1" style="will-change: transform; transform: perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1);">
